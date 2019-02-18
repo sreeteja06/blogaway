dea573e0fea9e7991db18ddd4e17fd26e78290a6
Sree Teja <sreeteja.muthyala@gmail.com>
2019-02-18 11:15:38 +0530

    article page design

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -16,14 +16,15 @@ class Article extends Component {
     render() {
         console.log("this.props");
         return (
-          <div>
+          <div class="article_container">
             <div className="article_Title" >Article Title</div>
+            <div className="article_content">this is where all the content will be stored. wish u a very happy and prosperous journey</div>
             <div className="article_line" />
-            <div className="article_authorText">author</div>
+            <div className="article_authorText">Author</div>
             <div className="article_authorID">
               {this.props.match.params.articleID}
             </div>
-            <div className="article_likesText">likes</div>
+            <div className="article_likesText">Likes</div>
             <div className="article_likesCount">
               {this.props.match.params.articleID}
             </div>
